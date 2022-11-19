5d90e2a0751a0235c5000785f7a89581c0037ac0
seregey gornih <you@example.com>
2022-11-19 21:09:27 +0300

    init cors

--- index.js
+++ index.js
@@ -91,7 +91,9 @@ app.use(express.static(__dirname + "/"));
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 
